403094a294fbbe05e02829bf6e3a6cd6e31a2db3
Yves <yveskayeye30@gmail.com>
2022-03-10 18:33:39 +0200

    database models

---
+++ app/models/tech.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean,DateTime
+
+from ..db.database import Base
+
+
+class Tech(Base):
+    __tablename__ = "Techs"
+
+    tech_id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    name = Column(String)
+    title = Column(String)
+    jobs_responded = Column(Integer)
+    is_superuser = Column(Boolean(), default=False)
+
+
+class TechAdmin(Base):
+    __tablename__ = "Tech_admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date_created = Column(DateTime, index=True)
+    tech_id = Column(Integer, ForeignKey("Tech.tech_id"))
+
+
+class TechJob(Base):
+    __tablename__ = "Tech_jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tech_id = Column(Integer, ForeignKey("Tech.tech_id"))
+    assigned_by = Column(Integer, default=0)
+    description = Column(String)
+    date_created = Column(DateTime, index=True)
+    
